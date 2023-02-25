9d0dce7c5cf4719d672cd49a2e14e17bc975e4e0
ifazlayrabbi <ifazlayrabbi@gmail.com>
2023-02-26 03:27:32 +0600

    class

--- 2 js class/1_jsClass.js
+++ 2 js class/1_jsClass.js
@@ -1,15 +1,23 @@
 'use strict'
 // A 'JS class' is a template for objects.
 
-class Car {
+
+
+class Car {                     // user defined class
   constructor(name, year){
     this.name = name
     this.year = year
   }
+  func () {
+    console.log('Good work.')
+  }
 }
 const car1 = new Car('Toyota', 2022)
 const car2 = new Car('Audi', 2023)
+const car3 = new Car()
+
 console.log(car1.name)
+car3.func()
 
 
 
@@ -21,22 +29,22 @@ class Calculation {
 
   addition (num1, num2){
     const ADD = num1 + num2
-    return ADD
+    return '\nAddition = ' + ADD
   }
 
   subtraction (num1, num2){
     const SUB = num1 - num2
-    return SUB
+    return 'Subtraction = ' + SUB
   }
   
   multiplication (num1, num2){
     const MUL = num1 * num2
-    return MUL
+    return 'Multiplication = ' + MUL
   }
   
   division (num1, num2){
     const DIV = num1 / num2
-    return DIV
+    return 'Division = ' + DIV + '\n'
   }
 
 }
@@ -58,4 +66,23 @@ console.log(cal.addition(8, 2))
 console.log(cal.subtraction(8, 2))
 console.log(cal.multiplication(8, 2))
 console.log(cal.division(8, 2))
+
+
+
+
+
+
+
+
+
+const date = new Date()   // system defined class
+console.log(date.toLocaleString())
+
+const options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric'
+}
+console.log(date.toLocaleString('en-US', options))
 
